7f44c54fb03da5d2cadfd41889ddc2753d0b9337
mihail <misabitno@gmail.com>
2025-03-12 17:14:33 +0300

    lab2 functions prototypes added

--- public/index.php
+++ public/index.php
@@ -1,2 +1,34 @@
 <?php
+function parseListOfCities($str):array 
+{
+    $cities = explode( ",", $str, PHP_INT_MAX);
+    $result = array();
+    foreach ($cities as $city)
+    {
+        $canAdd = true;
+        $city = trim($city);
+        if (!ctype_upper($city[0]))
+        {
+            $canAdd = false;
+        }
+        for ($i = 1; $i < strlen($city); $i++)
+        {
+            if (ctype_upper($city[$i]))
+            {
+                $canAdd = false;
+            }
+        }
+        if ($canAdd)
+        {
+            array_push($result, $city);
+        }
+    }
+    return array_unique($result, SORT_STRING);
+}  
+
+function sortCities($array):array
+{
+    natcasesort($array);
+    return $array;
+}
 phpinfo();
